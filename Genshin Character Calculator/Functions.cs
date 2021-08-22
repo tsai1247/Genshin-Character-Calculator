df4fa2d291034afd6af4ad13239772e586fa9702
tsai1247 <jeff29cc91079@gmail.com>
2021-08-22 15:19:37 +0800

    加入專案檔案。

---
+++ Genshin Character Calculator/Functions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Genshin_Character_Calculator
+{
+    
+    class Functions
+    {
+    }
+
+    public class FileRW
+    {
+        const string endl = "\r\n";
+
+        public static string[] ReadLineFile(String FilePath)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(FilePath, true);
+                string[] ans = reader.ReadToEnd().Split(endl);
+                reader.Close();
+                return ans;
+            }
+            catch (Exception)
+            {
+                return new string[] { };
+            }
+
+        }
+        public static void AppendLineFile(String FilePath, String Data)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, true);
+            writer.WriteLine(Data);
+            writer.Close();
+        }
+        public static void AppendLineFile(String FilePath, String[] SpiltData)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, true);
+            foreach (String str in SpiltData)
+                writer.WriteLine(str);
+            writer.Close();
+        }
+        public static void WriteLineFile(String FilePath, String Data)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, false);
+            writer.WriteLine(Data);
+            writer.Close();
+        }
+        public static void WriteLineFile(String FilePath, String[] SpiltData)
+        {
+            StreamWriter writer = new StreamWriter(FilePath, false);
+            foreach (String str in SpiltData)
+                writer.WriteLine(str);
+            writer.Close();
+        }
+        public static string Concat(string[] data, string concatSymbol = endl)
+        {
+            if (data.Length == 0)
+                return "";
+            string ret = data[0];
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (data[i] == "") { }
+                else if (data[i] == concatSymbol)
+                    ret += concatSymbol;
+                else
+                    ret += concatSymbol + data[i];
+            }
+            return ret;
+        }
+
+    }
+}
