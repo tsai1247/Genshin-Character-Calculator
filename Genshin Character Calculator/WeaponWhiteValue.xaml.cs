37be2c3e32a4430fd20cd91e9835fa32cf58d5b7
tsai1247 <jeff29cc91079@gmail.com>
2021-08-23 12:43:18 +0800

    [Add] a frame for input Weapon Data

---
+++ Genshin Character Calculator/WeaponWhiteValue.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Genshin_Character_Calculator
+{
+    /// <summary>
+    /// WeaponWhiteValue.xaml ç鈭åé頛
+    /// </summary>
+    public partial class WeaponWhiteValue : UserControl
+    {
+        public WeaponWhiteValue()
+        {
+            InitializeComponent();
+        }
+    }
+}
